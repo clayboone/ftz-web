84d7c13c5a4671384f7805fa61af8826cec0c7c7
Tener Hades <tener.hades@gmail.com>
2018-07-15 16:54:18 +0100

    auto

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { spawn } = require('child_process');
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const free = spawn('free', ['-m']);
+  const free = spawn('cat', ['/proc/meminfo']);
   let stdoutData = [];
   free.stdout.on('data', (data) => {
     stdoutData.push(data)
@@ -14,5 +15,20 @@ router.get('/', function(req, res, next) {
     res.render('index', { stdout: stdoutData });
   });
 });
+
+/**
+ * Get the amount of free system memory in bytes.
+ * 
+ * @returns {Number} Positive integer or zero
+ */
+function getFreeMem () {
+
+}
+
+/**
+ * Get the total amount of system memory, used and free.
+ * 
+ * @returns {Number} Positive integer or zero
+ */
 
 module.exports = router;
