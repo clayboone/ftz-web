5eb9314d6a22f51dfc048e777d2771c06e61c6a8
Tener Hades <tener.hades@gmail.com>
2018-07-15 17:12:06 +0100

    auto

--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,45 @@
 const express = require('express');
 const router = express.Router();
-const { spawn } = require('child_process');
 const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { stdout: "stdout data" });
+  const meminfo = getMeminfo();
+  res.render('index', { stdout: meminfo });
 });
 
-
+/**
+ * Build an object with data from /proc/meminfo
+ * 
+ * @returns {Object} Map-type object holding data from /proc/meminfo
+ */
+function getMeminfo() {
+  meminfoString = "";
+  fs.readFile('/proc/meminfo', (err, data) => {
+    if (err) return err;
+    meminfoString += data;
+  });
+  return meminfoString;
+}
 
 /**
  * Get the amount of free system memory in bytes.
  * 
+ * @param meminfo {Object} meminfo object
  * @returns {Number} Positive integer or zero
  */
-function getFreeMem () {
-
+function getFreeMem(meminfo) {
+  
 }
 
 /**
  * Get the total amount of system memory, used and free.
  * 
+ * @param meminfo {Object} meminfo object
  * @returns {Number} Positive integer or zero
  */
+function getTotalMem(meminfo) {
+
+}
 
 module.exports = router;
