d183beb08ec7a7fb4c8c009f858e0e8d75933f29
Tener Hades <tener.hades@gmail.com>
2018-07-15 16:01:33 +0100

    auto

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { spawn } = require('child_process');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const free = spawn('free', ['-m']);
+  let stdoutData = [];
+  free.stdout.on('data', (data) => {
+    stdoutData.push(data)
+  });
+
+  res.render('index', { stdout: stdoutData });
 });
 
 module.exports = router;
