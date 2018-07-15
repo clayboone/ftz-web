6cedd48f3d432d3a749e78bb883d80710bd7988a
Tener Hades <tener.hades@gmail.com>
2018-07-15 15:49:18 +0100

    auto

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const router = express.Router();
+const express = require('express');
+
+// const subprocess = require('subprocess')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
