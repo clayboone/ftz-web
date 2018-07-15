16e27a26a23d2014a60c056f47ac8201f72dc005
Tener Hades <tener.hades@gmail.com>
2018-07-15 17:02:01 +0100

    auto

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,10 @@ const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const free = spawn('cat', ['/proc/meminfo']);
-  let stdoutData = [];
-  free.stdout.on('data', (data) => {
-    stdoutData.push(data)
-  });
-
-  free.on('close', (exitCode) => {
-    res.render('index', { stdout: stdoutData });
-  });
+  res.render('index', { stdout: "stdout data" });
 });
+
+
 
 /**
  * Get the amount of free system memory in bytes.
