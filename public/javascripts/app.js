81814c53b7ad7795596db6de73041fe9730200ac
Tener Hades <tener.hades@gmail.com>
2018-07-17 17:29:41 +0100

    auto

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,10 +4,14 @@ setInterval(() => {
     counter++;
     const tag = document.getElementById("counter");
 
-    console.log(counter);
     tag.innerHTML = "<em>" + counter.toString() + "</em>";
 }, 100);
 
 (function () {
     console.log('getting meminfo...');
+    const tag = document.getElementById("meminfo");
+
+    const xhttp = new XMLHttpRequest();
+    temp = xhttp.open('GET', '/api/meminfo');
+    console.log(temp);
 })();
