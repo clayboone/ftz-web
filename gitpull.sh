002f6f436da78c4c63b01b475b3148b0133ec0a3
Tener Hades <tener.hades@gmail.com>
2018-07-17 16:18:08 +0100

    auto

--- gitpull.sh
+++ gitpull.sh
@@ -5,6 +5,6 @@ let couter=0 # bashism
 while (sleep 10)
 do {
     let counter++ # more bashisms
-    result=$(git pull | tail -n 1)
+    result=$(git pull | tail -n 1 2>/dev/null)
     echo "Pulled (${counter}): ${result}"
 } done
