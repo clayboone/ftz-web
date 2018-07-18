7275dc56e854b38167be7401acaff21e34b26cfe
Tener Hades <tener.hades@gmail.com>
2018-07-18 20:25:23 +0100

    auto

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,18 @@
 (function appMain() {
     const tag = document.getElementById("memusage");
-
     const xhttp = new XMLHttpRequest();
+
     xhttp.open('GET', '/api/meminfo');
     xhttp.send();
 
     xhttp.onreadystatechange = function() {
         meminfo = JSON.parse(this.response);
 
-        tag.setAttribute("max", meminfo["MemTotal"]);
-        tag.setAttribute("value", meminfo.MemTotal - meminfo.MemFree)
+        // <progress> attributes
+        // tag.setAttribute("max", meminfo["MemTotal"]);
+        // tag.setAttribute("value", meminfo.MemTotal - meminfo.MemFree)
+
+        let percentUsed = (meminfo.MemTotal - meminfo.MemFree) / meminfo.MemTotal;
+        tag.style.width = percentUsed;
     };
 })();
