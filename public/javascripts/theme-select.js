6f245b564ea0ed6b0e1428dd5ed7266c8f7c8ad5
Tener Hades <tener.hades@gmail.com>
2018-07-19 23:35:24 +0100

    auto

--- public/javascripts/theme-select.js
+++ public/javascripts/theme-select.js
@@ -1,7 +1,7 @@
 function changeTheme() {
     const body = document.getElementsByTagName('body');
 
-    if (document.querySelector('.clicked') == null) {
+    if (document.querySelector('clicked') == null) {
         console.log('no clicked class. adding it.');
         document.querySelector('body').classList.remove('clicked');
     } else {
