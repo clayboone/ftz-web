b0db2448bfde168474f45c1009214249c1a68230
Tener Hades <tener.hades@gmail.com>
2018-07-19 23:34:09 +0100

    auto

--- public/javascripts/theme-select.js
+++ public/javascripts/theme-select.js
@@ -1,3 +1,11 @@
 function changeTheme() {
-    console.log('Pressed');
+    const body = document.getElementsByTagName('body');
+
+    if (document.querySelector('.clicked') == null) {
+        console.log('no clicked class. adding it.');
+        document.querySelector('body').classList.remove('clicked');
+    } else {
+        console.log('clicked class found. removing it');
+        document.querySelector('body').classList.add('clicked');
+    }
 }
