b55a2da45a8c602995e42071e87c5dcd0fd45e3c
Tener Hades <tener.hades@gmail.com>
2018-07-21 12:00:04 +0100

    auto

--- app.js
+++ app.js
@@ -14,9 +14,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev', {
-  skip: (req, res) => { return res.statusCode < 400 }
-}));
+if (process.env.NODE_ENV !== 'production') {
+  app.use(logger('dev', {
+    skip: (req, res) => { return res.statusCode < 400 }
+  }));
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
