020bb272fc81d440ceeda20c19eb2eb47b437ae6
Tener Hades <tener.hades@gmail.com>
2018-07-22 23:30:52 +0100

    auto

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,10 @@ router.get('/api/meminfo', (req, res, next) => {
   });
 });
 
+router.get('/api/stat', (req, res, next) => {
+  getProcstat();
+});
+
 /**
  * Build an object with data from /proc/meminfo
  * 
@@ -32,16 +36,30 @@ function getMeminfo(callback) {
     }
 
     let meminfoObject = {};
-    data.toString().split('\n').forEach((value, index) => {
-      if (value.length > 0) {
-        let key = value.split(':')[0];
-        let val = Number(value.split(':')[1].trim().match(/^[0-9]+/)[0]);
+    data.toString().split('\n').forEach((line) => {
+      if (line.length > 0) {
+        let key = line.split(':')[0];
+        let val = Number(line.split(':')[1].trim().match(/^[0-9]+/)[0]);
 
         meminfoObject[key] = val;
       }
     });
 
     callback(meminfoObject);
+  });
+}
+
+function getProcstat(callback) {
+  fs.readFile('/proc/stat', (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    let result = {};
+    data.toString().split('\n').forEach((line) => {
+      const elements = line.split(/ +/).filter(Boolean);
+      console.log(elements);
+    });
   });
 }
 
