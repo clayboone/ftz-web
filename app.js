42943add7f3fbc72bd089d33a3b849255934b1cb
Tener Hades <tener.hades@gmail.com>
2018-07-26 16:18:16 +0100

    removed users route

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var lessMiddleware = require('less-middleware');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -27,7 +26,6 @@ app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
