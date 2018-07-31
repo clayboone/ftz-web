be2cd27da384ef93c8582fc83cd9edef1dfd9d6b
Tener Hades <tener.hades@gmail.com>
2018-07-31 10:46:37 +0100

    auto

--- public/javascripts/go-home.js
+++ public/javascripts/go-home.js
@@ -1,3 +1,13 @@
+/**
+ * Wait half a second, then zoom logo into view.
+ */
+setTimeout(function () {
+
+}, 500);
+
+/**
+ * Then go to real home page.
+ */
 setTimeout(function() {
     window.open('/home', '_self');
-}, 1000);
+}, 2000);
